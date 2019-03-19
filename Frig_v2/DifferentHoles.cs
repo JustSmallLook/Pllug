4ca10fe8a09d595ea3f2572b4f462d91e3ca1f84
Volodymyr Karpliak <43876856+JustSmallLook@users.noreply.github.com>
2019-03-19 14:46:47 +0200

    Add files via upload

---
+++ Frig_v2/DifferentHoles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frig_v2
+{
+    public class DifferentHoles
+    {
+        public void Rectangular(out double heightHole, out double widthHole)
+        {
+            Console.WriteLine("You have selected rectangular hole!");
+            UserInput input = new UserInput();
+
+            heightHole = input.ObjectSize("height");
+            widthHole = input.ObjectSize("width");
+        }
+        public void Round(out double diameterHole)
+        {
+            Console.WriteLine("You have selected round hole!");
+            UserInput input = new UserInput();
+
+            diameterHole = input.ObjectSize("diamater");
+
+        }
+
+    }
+}
